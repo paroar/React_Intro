d84a24ddcff534fc58148eb419775e9945d64d80
paroar <prodriguezarmida@gmail.com>
2019-10-20 19:14:08 +0200

    todo list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  tasks = [
+    { id: 1, name: "Ir a clase", completed: false },
+    { id: 2, name: "Descansar", completed: true }
+  ];
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Todo List</h2>
+        <ul>
+          {this.tasksList()}
+        </ul>
+        <input id="input-add" type="text" placeholder="Task here..." />
+        <button onClick={this.addTask}>Add</button>
+      </div>
+    );
+  }
+
+  tasksList() {
+    return (
+      <>
+        {this.tasks.map(
+          ({ id, name, completed }) =>
+            <>
+              <li key={id}><input type="checkbox" />{name}</li>
+            </>
+        )}
+      </>
+    )
+  }
+
+  addTask() {
+    const input = document.querySelector("#input-add").value;
+    this.tasks.push({ id: this.tasks.length, name: input, completed: false });
+  }
 }
 
-export default App;
+
+export { App };
